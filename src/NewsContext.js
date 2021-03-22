979f9d47ddfb52947a37842e13cd0d4e0f4280b7
timng001 <68362433+timng001@users.noreply.github.com>
2021-03-22 00:23:33 -0400

    Add files via upload

---
+++ src/NewsContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+export const NewsContext = createContext();
+
+export const NewsContextProvider = (props) => {
+  const [data, setData] = useState();
+  const apiKey = "f63646f9b54540d191a6b8189720bc50";
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://newsapi.org/v2/everything?q=bitcoin&from=2021-03-13&sortBy=publishedAt&apiKey=${apiKey}`
+      )
+      .then((response) => setData(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <NewsContext.Provider value={{ data }}>
+      {props.children}
+    </NewsContext.Provider>
+  );
+};
